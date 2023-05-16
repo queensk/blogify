e37717fcf71ca6c92ff135adeae50f16a75338b0
queens kisivuli <queenskisivuli@gmail.com>
2023-05-16 20:14:10 +0300

    added blogs

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 
 export default function Blog({ title, date, image, article }) {
+  const paragraphs = article.split("\n");
   return (
     <div className="blog-data">
       <div className="blog-header">
         <h1>{title}</h1>
         <h2> published {date}</h2>
       </div>
-
-      <img
-        src={image}
-        alt="Blog"
-      />
+      <img src={image} alt="Blog" />
       <div className="blog-body">
-        <p>{article}</p>
+        {paragraphs.map((paragraph, index) => (
+          <>
+            <p key={index}>{paragraph}</p>
+            <br />
+          </>
+        ))}
       </div>
     </div>
   );
