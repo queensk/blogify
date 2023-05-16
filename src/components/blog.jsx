3d981f4d77bea0e72dcf062a5104ca64a34ab7bb
queens kisivuli <queenskisivuli@gmail.com>
2023-05-16 13:21:41 +0300

    init app

---
+++ src/components/blog.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Blog({ title, date, image, article }) {
+  return (
+    <div className="blog-data">
+      <div className="blog-header">
+        <h1>{title}</h1>
+        <h2>{date}</h2>
+      </div>
+      <img src={image} alt="Blog" />
+      <div className="blog-body">
+        <p>{article}</p>
+      </div>
+    </div>
+  );
+}
