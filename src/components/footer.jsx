652c9362c247329b7eaecf7ed142f863f6259919
queens kisivuli <queenskisivuli@gmail.com>
2023-05-18 12:48:40 +0300

    footer component

---
+++ src/components/footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function footer() {
+  return (
+    <footer>
+      <div class="footer-content">
+        <p>
+          &copy; {new Date().getFullYear().toString()} Blogify. All rights
+          reserved. | Today's date {new Date().toLocaleDateString()}
+        </p>
+        <p>Contact: Blogify@email.com</p>
+        <p>By Queens Kisivuli</p>
+      </div>
+    </footer>
+  );
+}
