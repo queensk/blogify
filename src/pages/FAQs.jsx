7cbcc098c65266f358187cc6ce86b1869d52439c
queens kisivuli <queenskisivuli@gmail.com>
2023-05-18 22:51:09 +0300

    frequent messages

---
+++ src/pages/FAQs.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function FAQs() {
+  return (
+    <div className="faqs-container">
+      <h2>Frequently Asked Questions</h2>
+      <div className="faq-item">
+        <h3>What topics do you cover on Blogify?</h3>
+        <p>
+          We cover a wide range of topics including technology, lifestyle,
+          travel, fashion, health, and more. Our goal is to provide diverse and
+          engaging content that caters to various interests.
+        </p>
+      </div>
+      <div className="faq-item">
+        <h3>How can I contribute to Blogify?</h3>
+        <p>
+          We welcome guest posts and collaborations. If you have a story to
+          tell, knowledge to share, or expertise to contribute, please reach out
+          to our team through the contact information provided.
+        </p>
+      </div>
+      <div className="faq-item">
+        <h3>How often do you publish new articles?</h3>
+        <p>
+          We strive to publish new articles regularly. Our team is dedicated to
+          bringing you fresh and informative content on a consistent basis.
+        </p>
+      </div>
+      <div className="faq-item">
+        <h3>Can I leave comments on articles?</h3>
+        <p>
+          Absolutely! We encourage readers to engage in discussions and share
+          their thoughts in the comments section of our articles. Your feedback
+          and insights are highly valued.
+        </p>
+      </div>
+    </div>
+  );
+}
